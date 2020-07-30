8045d865d45ae3c95179b69d2f6c3870b0a07a21
Purushottam Singh <purushottamsingh@PurushottamsMBP.home>
2020-07-30 11:59:41 -0400

    filter

--- src/main/java/com/example/demo/Services/CategoryService.java
+++ src/main/java/com/example/demo/Services/CategoryService.java
@@ -8,6 +8,8 @@ import com.example.demo.Models.Category;
 import com.example.demo.Repositories.CategoryRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -23,10 +25,15 @@ public class CategoryService {
      
     public List<Category> listAll(int pageNo, int pageSize, String orderBy, String orderDirection) {
         Pageable page = PageRequest.of(pageNo,pageSize,Sort.by(Direction.valueOf(orderDirection), orderBy));
-        return repo.findAll(page).getContent();
+        Category ct = new Category();
+        ct.setName("c");
+        ExampleMatcher mt = ExampleMatcher.matching().withMatcher("name", m-> m.contains());
+        Example<Category> e = Example.of(ct,mt);
+        return repo.findAll(e,page).getContent();
     }
     
     public List<Category> listAll() {
+        
         return repo.findAll();
     }
      
